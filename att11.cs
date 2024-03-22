03598247db24fc102697810881bb7c2b03d70c42
Pedro Henrique Rodrigues Evangelista <99209592+Pedro125600@users.noreply.github.com>
2024-03-22 19:46:56 -0300

    Update att11.cs

--- att11.cs
+++ att11.cs
@@ -4,29 +4,39 @@ class Program
 {
     public static void Main(string[] args)
     {
-      Console.WriteLine("Digite uma letra:");
-      char letra = char.Parse(Console.ReadLine());
+        Console.WriteLine("Digite uma letra:");
+        char letra = char.Parse(Console.ReadLine());
 
-      switch (letra)
-      {
-          case 'a':
-              Console.WriteLine("é uma vogal.");
-              break;
-               case 'e':
-              Console.WriteLine("é uma vogal.");
-              break;
-               case 'i':
-              Console.WriteLine("é uma vogal.");
-              break;
-               case 'o':
-              Console.WriteLine("é uma vogal.");
-              break;
-               case 'u':
-              Console.WriteLine("é uma vogal.");
-              break;
-          default:
-              Console.WriteLine("é uma consoante.");
-              break;
-      }
+        int cont = (int)letra;
+
+        if( cont >= 65 && cont<=90)
+        {
+            letra = (char)(letra + 32);
+        }
+        
+
+
+        switch (letra)
+        {
+            case 'a':
+                Console.WriteLine("é uma vogal.");
+                break;
+            case 'e':
+                Console.WriteLine("é uma vogal.");
+                break;
+            case 'i':
+                Console.WriteLine("é uma vogal.");
+                break;
+            case 'o':
+                Console.WriteLine("é uma vogal.");
+                break;
+            case 'u':
+                Console.WriteLine("é uma vogal.");
+                break;
+            default:
+                Console.WriteLine("é uma consoante.");
+                break;
+        }
+        Console.ReadLine();
     }
 }
