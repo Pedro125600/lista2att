83b18a4fd3c09fcbc6ade6f26e1ce9829dd52b0d
Pedro Henrique Rodrigues Evangelista <99209592+Pedro125600@users.noreply.github.com>
2024-03-22 19:23:09 -0300

    Update att13.cs

--- att13.cs
+++ att13.cs
@@ -4,13 +4,12 @@ class Program
 {
     public static void Main(string[] args)
     {
-      Console.WriteLine("Digite um ano para verificar se é bissexto: ");
-      int ano = int.Parse(Console.ReadLine());
+   
+            Console.WriteLine("Digite um ano");
 
-      bool bissexto = (ano % 400 == 0) || (ano % 4 == 0 && ano % 100 != 0);
+            int ano = int.Parse(Console.ReadLine());
 
-      string res = bissexto ? "é bissexto." : "não é bissexto.";
+            Console.WriteLine  (((ano % 400 == 0) || (ano % 4 == 0 && ano % 100 != 0)) ? "Ano bissexto" : "Ano não e bissexto");
 
-      Console.WriteLine(res);
-
+            Console.ReadLine();
 }
